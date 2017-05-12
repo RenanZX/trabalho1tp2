79b0adf6b25f8ff24b6df1df9eb86c64193efdc8
Renan Godoi <renangodoimedeiros@gmail.com>
2017-05-12 00:21:04 -0300

    Update++
    agora possui interfaces que interagem com o usuario

---
+++ include/ControleInterfaceUsuario.h
@@ -0,0 +1,31 @@
+#ifndef CONTROLEINTERFACEUSUARIO_H
+#define CONTROLEINTERFACEUSUARIO_H
+
+#include "interfaces.h";
+#include "GerenciadorQuiz.h"
+
+class InterfaceUserGerQuiz:public InterfaceUsuarioGerQuiz/*interfaces que irão interagir com o usuario*/
+{
+private:
+    static const int ADICIONARP;/*constantes*/
+    static const int DELELETARP;
+    static const int EDITARP;
+    static const int SAIRP;
+    static const int SAIR;
+    static const int SELQUIZ;
+    static const int LOGQUIZ;
+    static const int DELQUIZ;
+    static const int EDITQUIZ;
+    static const int CREATEQUIZ;
+    void EditarQuiz()throw(runtime_error);
+public:
+    void executar()throw(runtime_error);
+};
+//falta implementar
+class InterfaceUserQuiz:public InterfaceUsuarioQuiz
+{
+public:
+    void executar()throw(runtime_error);
+};
+
+#endif // CONTROLEINTERFACEUSUARIO_H
--- include/GerenciadorQuiz.h
+++ include/GerenciadorQuiz.h
@@ -21,34 +21,22 @@ class GerQuiz /*gerenciador de quiz*/
 {
 private:
     void PegaAttr(string dado);/*captura atributos de dados lidos do arquivo com quiz*/
-    void AddPerguntas(); /*adiciona perguntas ao quiz setado em quest√£o*/
     void ReorganizarPerArq(string editind,string novap);/*reorganiza o arquivo quiz com as perguntas*/
     string FormularPergArq(string form1,string form2,string form3); /*formula as perguntas no arquivo*/
-    void EditPer();/*editor de perguntas no arquivo do quiz*/
-    void DelPer();/*deleta perguntas do arquivo com quiz*/
     string BuscPer(string ind);/*busca a pergunta no arquivo atravez de um indice*/
     string data1,data2,data3;/*data1=index de uma pergunta data2=pergunta data3=resposta*/
     string arquivo,diretorio;/*nome do arquivo e nome do diretorio*/
-    static const int ADICIONARP;/*constantes*/
-    static const int DELELETARP;
-    static const int EDITARP;
-    static const int SAIRP;
-    static const int SAIR;
-    static const int SELQUIZ;
-    static const int LOGQUIZ;
-    static const int DELQUIZ;
-    static const int EDITQUIZ;
-    static const int CREATEQUIZ; /*metodos exclusivos e privados da classe*/
-    void CriarQuiz();/*metodo que cria o arquivo contendo o quiz*/
-    bool carregaQuiz();/*metodo que carrega o arquivo contendo o quiz*/
-    void CarregaQuizEd();/*metodo que tenta carregar o quiz previamente setado*/
-    void SelecionarArq();/*seleciona o arquivo contendo o quiz*/
-    void EditarQuiz(); /*edita o arquivo contendo o quiz*/
-    void ApagarQuiz(); /*apaga o arquivo contendo o quiz*/
 public:
     GerQuiz(); /*construtor*/
     ~GerQuiz();
-    void executar(); /*interface com o usuario*/
+    void CriarQuiz() throw(invalid_argument);/*metodo que cria o arquivo contendo o quiz*/
+    bool carregaQuiz();/*metodo que carrega o arquivo contendo o quiz*/
+    void CarregaQuizEd()throw(invalid_argument);/*metodo que tenta carregar o quiz previamente setado*/
+    void SelecionarArq()throw(invalid_argument);/*seleciona o arquivo contendo o quiz*/
+    void ApagarQuiz()throw(invalid_argument); /*apaga o arquivo contendo o quiz*/
+    void AddPerguntas()throw(invalid_argument); /*adiciona perguntas ao quiz setado em quest„o*/
+    void EditPer()throw(invalid_argument);/*editor de perguntas no arquivo do quiz*/
+    void DelPer()throw(invalid_argument);/*deleta perguntas do arquivo com quiz*/
     string getQuizFileName();/*retorna o nome do ultimo arquivo setado*/
     string getFolderFilesName(); /*retorna a pasta contendo os quizes*/
 };
