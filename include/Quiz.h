8857fe8269b213ea741793ceb4f6b72ff38df31c
Renan Godoi <renangodoimedeiros@gmail.com>
2017-06-24 13:41:47 -0300

    Add files via upload

--- include/Quiz.h
+++ include/Quiz.h
@@ -3,23 +3,43 @@
 #include "GerenciadorQuiz.h"
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <sstream>
 #include <ctype.h>
+#include <vector>
+/*#include <tempo.h>*/
 
 using namespace std;
+
+class Resposta
+{
+protected:
+    int respostacorreta;
+    std::vector<string> opcoes;
+public:
+    ~Resposta();
+    void SetRespostasArquivo(string respostas);
+    void MostrarOpcoes();
+    bool VerificarRespostaCorreta(int resposta);
+    bool CompararResposta(Resposta res);
+    int getRespostaCorreta();
+};
 
 class Pergunta/*classe de perguntas*/
 {
 private:
     string index;/*indice da pergunta*/
     string strpergunta;/*pergunta*/
-    string strresposta; /*resposta*/
+    Resposta resposta;
 public:
-    void SetPergunta(string ind,string per,string res) throw(invalid_argument); /*seta a pergunta*/
+    void SetPergunta(string ind,string per,Resposta *res) throw(invalid_argument); /*seta a pergunta*/
     string getIndex();/*retorna o indice da pergunta*/
     string getPergunta(); /*retorna a pergunta*/
-    string getResposta(); /*retorna a resposta da pergunta*/
+    Resposta getResposta(); /*retorna a resposta da pergunta*/
+    void ImprimirOpcoes();
     bool Comparar(Pergunta p); /*compara duas perguntas*/
-    bool CompararResposta(string resposta);
+    bool CompararResposta(int respostaverificar);
+    int getRespostaCorreta();
 };
 
 class listaPergunta;
@@ -59,14 +79,17 @@ private:
     const static int PULAR;
     const static int SAIR;
     void OrganizarLista(string inds);/*organiza a lista apartir de uma string de indices*/
+    string topicosdisciplinas;
+    string file;
 public:
     Quiz();/*construtor*/
     virtual ~Quiz();/*destrutor*/
     void ComecarQuiz()throw(invalid_argument);/*comeca um quiz*/
     void CarregarQuiz()throw(invalid_argument);/*carrega um quiz existente*/
     void SelecionarPergunta()throw(invalid_argument); /*seleciona as perguntas para o usuario*/
-    void SetPergunta(string in,string p,string r)throw(invalid_argument); /*seta as perguntas para o usuario*/
+    void SetPergunta(string in,string p,Resposta r)throw(invalid_argument); /*seta as perguntas para o usuario*/
     void lerQuiz()throw(invalid_argument);/*lê o quiz*/
+    void selectTopicosDisciplinas()throw(invalid_argument) /*seleciona o topico e a disciplina a qual o usuario ira responder*/;
 };
 
 #endif // QUIZ_H
